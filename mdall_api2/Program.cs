e0f3bcd793c3d714970b7882b5b54a1a2f395749
Cynthia <Cynthia@DESKTOP-8COKEOE>
2022-06-04 21:22:21 -0400

    client files and models

--- mdall_api2/Program.cs
+++ mdall_api2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using mdall_api2.Models;
+using Newtonsoft.Json;
+
 Console.WriteLine("Hello, World!");
+
+var client = new HttpClient();
+client.DefaultRequestHeaders.Add("user-key", "8f9d5d7deef1c93d908bf793a62e7ade");
+client.BaseAddress = new Uri("https://mdall-hc-sc-apicast-production.api.canada.ca/v1/");
+var response = await client.GetAsync("company?lang=en&type=json&id=113585");
+var content = await response.Content.ReadAsStringAsync();
+var company = JsonConvert.DeserializeObject<Company>(content);
+
+response = await client.GetAsync("licence?lang=en&type=json&company_id=113585");
+content = await response.Content.ReadAsStringAsync();
+var licence = JsonConvert.DeserializeObject<List<Licence>>(content);
+
+
+
